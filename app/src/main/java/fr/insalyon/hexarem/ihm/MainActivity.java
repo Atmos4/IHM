ffa1ad5615cc2ecaa3bc3f1702abdb062fef2354
Arnaud <arnaud.perrin@insa-lyon.fr>
2018-01-02 13:53:33 +0100

    Ajout de la toolbar et du bouton paramètres

--- app/src/main/java/fr/insalyon/hexarem/ihm/MainActivity.java
+++ app/src/main/java/fr/insalyon/hexarem/ihm/MainActivity.java
@@ -8,11 +8,21 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    /**
+     * La barre d'outils en haut de l'écran
+     */
+    Toolbar myToolbar;
+    /**
+     * La barre de navigation en bas de l'écran
+     */
     private BottomNavigationView bottomNavigation;
     private Fragment fragment;
     private FragmentManager fragmentManager;
@@ -21,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigation = (BottomNavigationView)findViewById(R.id.navigation);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        bottomNavigation = findViewById(R.id.navigation);
         fragmentManager = getSupportFragmentManager();
         bottomNavigation.setSelectedItemId(R.id.navigation_home);
         fragment = new HomeFragment();
@@ -47,6 +59,31 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                Toast.makeText(this,"Paramètres !",Toast.LENGTH_LONG).show();
+                return true;
+
+
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
 }
